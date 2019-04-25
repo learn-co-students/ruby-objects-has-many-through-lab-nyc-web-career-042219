c9f519b06bfba1f52cfc00d884e1d5d29ed37a8e
Adam Sultanov <adam.sultanov@gmail.com>
2019-04-25 17:38:46 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,32 @@
+require 'pry'
+class Artist
+  @@all = []
+
+  attr_accessor :name
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def genres
+    songs.map do |song|
+      song.genre
+    end
+  end
+
+end
---
+++ lib/doctor.rb
@@ -0,0 +1,33 @@
+require 'pry'
+
+class Doctor
+  attr_reader :name
+  @@all = []
+  @appointments = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(patient, self, date)
+  end
+
+  def appointments
+    Appointment.all.select do |app|
+      app.doctor == self
+    end
+  end
+
+  def patients
+    appointments.map do |app|
+      app.patient
+    end
+  end
+
+end
