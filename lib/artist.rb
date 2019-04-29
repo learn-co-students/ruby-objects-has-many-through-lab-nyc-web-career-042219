8c5740dfa4369b212c142fc7c95bbae0b48c0c8b
Jessica Lin <jessicaa.lin@gmail.com>
2019-04-29 01:31:20 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,32 @@
+require 'pry'
+
+class Artist
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+
+  def songs
+    Song.all.select { |song| song.artist == self }
+  end
+
+  def genres
+    songs.map { |song| song.genre }
+  end
+
+end
+
+# binding.pry
